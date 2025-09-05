cca0310701861dd68512c418fc39ce6408b217ad
I578443 <sathyabhama.shibu@sap.com>
2025-09-05 15:05:18 +0530

    added

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 def add(a, b):
     return a + b
 
+def sub(a,b):
+    return a - b
+
+def div(a,b):
+    return a / b
 if __name__ == "__main__":
     print("App running... sum(2,3) =", add(2,3))
+    print("App running... subtract(6,3) =", sub(6,3))
+    print("App running... divide(6,3) =", sub(6,3))
--- test_app.py
+++ test_app.py
@@ -4,6 +4,12 @@ import app
 class TestApp(unittest.TestCase):
     def test_add(self):
         self.assertEqual(app.add(2, 3), 5)
+    
+    def test_sub(self):
+        self.assertEqual(app.sub(6,3), 3)
+
+    def test_div(self):
+        self.assertEqual(app.div(6,3), 2)
 
 if __name__ == '__main__':
     unittest.main()
